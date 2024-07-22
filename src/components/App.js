cc6e5f609c766a7ed14d3d16715c6f439cc72bee
Vishnu tyagi <106895072+Vishnutyagi@users.noreply.github.com>
2024-07-22 17:08:04 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [name,setName]=useState("");
+  const changed=(e)=>{
+    setName(e.target.value);
+    e.preventDefault();
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+        <form>
+          <input type="text" onChange={changed}/>
+          {<div>Hello {name}</div>}
+        </form>
     </div>
   )
 }
